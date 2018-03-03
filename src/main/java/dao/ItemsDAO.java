2287c5a7ade35e87ac5365ff073775250baa3068
fzj <fengzongjian@yeah.net>
2018-03-03 16:48:17 +0800

    商品详细信息展示

--- src/main/java/dao/ItemsDAO.java
+++ src/main/java/dao/ItemsDAO.java
@@ -57,6 +57,54 @@ public class ItemsDAO {
 				}
 			}
 		}
+	}
 
+	// 根据商品编号获得商品资料
+	public Items getItemsById(int id) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBHelper.getConnection();
+			String sql = "select * from items where id=?;"; // SQL语句
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				Items item = new Items();
+				item.setId(rs.getInt("id"));
+				item.setName(rs.getString("name"));
+				item.setCity(rs.getString("city"));
+				item.setNumber(rs.getInt("number"));
+				item.setPrice(rs.getInt("price"));
+				item.setPicture(rs.getString("picture"));
+				return item;
+			} else {
+				return null;
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		} finally {
+			// 释放数据集对象
+			if (rs != null) {
+				try {
+					rs.close();
+					rs = null;
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+			// 释放语句对象
+			if (stmt != null) {
+				try {
+					stmt.close();
+					stmt = null;
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
 	}
 }
